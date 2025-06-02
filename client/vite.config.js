ca0f9bbb935354e772ccbb4ce77a6a666c4a4573
Zhong <zhtgeneral@gmail.com>
2025-06-02 13:38:48 -0700

    revert config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        /* This sets the entry point */
-        app: './src/index.jsx' 
-      }
-    }
-  }
+  plugins: [react()]
 })
