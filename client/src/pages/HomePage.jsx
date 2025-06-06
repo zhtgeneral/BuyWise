68c8306d758dc86253c148edfba41cab1fd458a7
dkhanna99 <dhruvkhanna1104@gmail.com>
2025-06-06 14:46:09 -0700

    updated CSS

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,10 +2,13 @@
 /**
  * This is the entry point of our app located at `/`
  */
+import '../styles/HomePage.css'; 
+
 export default function HomePage() {
-  return (
-    <main>
-      Welcome to BuyWise! Please Enjoy Our Demo!
-    </main>
-  )
+    return (
+        <div className="main-content">
+            <h1>Welcome to <span className="highlight">BuyWise</span>!</h1>
+            <p>Please Enjoy Our Demo!</p>
+        </div>
+    );
 }
